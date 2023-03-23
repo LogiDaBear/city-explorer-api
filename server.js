73f9cb1a47d1ca93ebadfbfb998e6d5c9dc598f8
Logan Reese <lreese2112@gmail.com>
2023-03-23 00:55:47 +0000

    movies and weather

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 'use strict';
+const axios = require('axios');
 
 console.log('Zup bone daddy...?')
 
@@ -45,20 +46,64 @@ app.get('/hello', (request, response) => {
   response.status(200).send(`Howdy ${userFirstName} ${userLastName}! Welcome to my server.`);
 
 });
+//TODO: Build an endpoint that will call out to an api
+// app.get('/', async(request, response, next) => {
+//   try{
+//TODO: accept queries -> /movies?searchQuery=value
 
-app.get('/weather', (request, response, next) => {
+//     let keywordFromFrontEnd = request.query.searchQuery;
+
+//TODO: build my url for axios
+//     let url = `${request.query}`
+
+// let movieResults = await axios.get(url);
+
+//TODO: groom that data and send it to the frontend
+//let moviesToSend = movieResults.data.results.map((movie => new Movie(movie)));
+//   }
+// }
+app.get('/movies', async (request, response, next) => {
+  try {
+    let queriedMovie = request.query.title;
+    let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_API_KEY}&language=en-US&query=${queriedMovie}&include_adult=false`
+    console.log(movieUrl);
+
+    let movieResultsfromAxios = await axios.get(movieUrl);
+    console.log(movieResultsfromAxios);
+
+    let moviecityResults = movieResultsfromAxios.data.results.map((movie) => new Movies(movie));
+
+    response.status(200).send(moviecityResults);
+  } catch (error) {
+    next(error);
+  }
+});
+
+class Movies {
+  constructor(mObj) {
+    this.title = mObj.title;
+    this.overview = mObj.overview;
+  }
+};
+
+
+app.get('/weather', async (request, response, next) => {
   try {
     let queriedLat = request.query.lat;
     let queriedLon = request.query.lon;
-    console.log(queriedLat, queriedLon);
-    let foundCity = data.find(weather => {
-      console.log(weather.lat, weather.lon)
-    return weather.lat == queriedLat && weather.lon == queriedLon})
-    console.log(foundCity);
-    let cityForecast = foundCity.data.map((day, idx) => new Forecast(day));
-    
-      
-      
+    let weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${queriedLat}&lon=${queriedLon}`
+    console.log(weatherUrl);
+    let weatherDatafromAxios = await axios.get(weatherUrl);
+    // let foundCity = data.find(weather => {
+    console.log(weatherDatafromAxios);
+
+    // return weather.lat == queriedLat && weather.lon == queriedLon})
+    // console.log(foundCity);
+    let cityForecast = weatherDatafromAxios.data.data.map((day, idx) => new Forecast(day));
+    console.log(cityForecast);
+
+
+
 
     response.status(200).send(cityForecast);
   } catch (error) {
@@ -71,7 +116,7 @@ class Forecast {
     this.date = obj.valid_date,
       this.description = obj.weather.description
   }
-}
+};
 
 
 
