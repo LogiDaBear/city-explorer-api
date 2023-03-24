807519bdcc301a537ab21a155639de7d6d424453
Logan Reese <lreese2112@gmail.com>
2023-03-24 22:38:26 +0000

    server cache

--- modules/movies.js
+++ modules/movies.js
@@ -2,19 +2,39 @@
 
 const axios = require('axios');
 
+let moviesCache = {};
+
 async function getMovies (request, response, next) {
   try {
     let queriedMovie = request.query.title;
+
+    let moviesKey = `${queriedMovie}-Movie`;
     
-    let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_API_KEY}&language=en-US&query=${queriedMovie}&include_adult=false`
-    console.log(movieUrl);
+    if(moviesCache[moviesKey] && (Date.now() - moviesCache[moviesKey].timestamp) < 8.64e+7){
+      console.log('moviesCACHE HIT FOR CASH!', moviesCache);
 
-    let movieResultsfromAxios = await axios.get(movieUrl);
-    console.log(movieResultsfromAxios);
+      response.status(200).send(moviesCache[moviesKey].data);
 
-    let moviecityResults = movieResultsfromAxios.data.results.map((movie) => new Movies(movie));
+    }else{
 
-    response.status(200).send(moviecityResults);
+      console.log('NO ITEMS FOR CASH!');
+
+      let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_API_KEY}&language=en-US&query=${queriedMovie}&include_adult=false`
+
+      let movieResultsfromAxios = await axios.get(movieUrl);
+      console.log(movieResultsfromAxios);
+  
+      let moviecityResults = movieResultsfromAxios.data.results.map((movie) => new Movies(movie));
+
+      //****BUILD INTO moviesCACHE */
+
+      moviesCache[moviesKey] = {
+        data: moviecityResults,
+        timestamp: Date.now()
+      };
+
+      response.status(200).send(moviecityResults);
+    }
   } catch (error) {
     next(error);
   }
--- modules/weather.js
+++ modules/weather.js
@@ -2,23 +2,38 @@
 
 const axios = require('axios');
 
+let weatherCache = {};
+
 async function getWeather(request, response, next) {
   try {
     let queriedLat = request.query.lat;
-
     let queriedLon = request.query.lon;
+    let weatherKey = `${queriedLat, queriedLon}-Weather`;
 
-    let weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${queriedLat}&lon=${queriedLon}`
+    if(weatherCache[weatherKey] && (Date.now() - weatherCache[weatherKey].timestamp) < 8.64e+7){
+      console.log('weatherCACHE HIT!',weatherCache);
 
-    console.log(weatherUrl);
+      response.status(200).send(weatherCache[weatherKey].data);
 
-    let weatherDatafromAxios = await axios.get(weatherUrl);
-    console.log(weatherDatafromAxios);
+    }else{
+      console.log('weatherCACHE NOT HIT :(');
 
-    let cityForecast = weatherDatafromAxios.data.data.map((day, idx) => new Forecast(day));
-    console.log(cityForecast);
+      let weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${queriedLat}&lon=${queriedLon}`
+  
+      let weatherDatafromAxios = await axios.get(weatherUrl);
+      console.log(weatherDatafromAxios);
+  
+      let cityForecast = weatherDatafromAxios.data.data.map((day, idx) => new Forecast(day));
+      console.log(cityForecast);
 
-    response.status(200).send(cityForecast);
+      weatherCache[weatherKey] = {
+        data: cityForecast,
+        timestamp: Date.now()
+      };
+  
+      response.status(200).send(cityForecast);
+    }
+
   } catch (error) {
     next(error);
   }
